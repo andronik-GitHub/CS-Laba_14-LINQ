ec99bf54acd6f4c07550491924d2d42ba26cbfcd
kazmi <costellationoblivion@gmail.com>
2022-11-19 09:41:13 +0200

    Добавьте файлы проекта.

--- Ex8/Ex8.cs
+++ Ex8/Ex8.cs
@@ -1,8 +1,8 @@
-using System;
+﻿using System;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-class Ex7
+class Ex8
 {
     static void Main()
     {
@@ -16,17 +16,17 @@ class Ex7
                 if (action[0].ToLower() == "end") { Console.WriteLine(); break; } // завершення вводу
                 else if (action.Length >= 3) // якщо введено 3 слова
                 {
-                    int[] temp = new int[action.Length - 2];
-                    for (int i = 2; i < action.Length; i++)
-                        temp[i] = Convert.ToInt32(action[i]);
+                    int[] temp = new int[action.Length - 2]; // для запису оцінок
+                    for (int i = 0; i < temp.Length; i++)
+                        temp[i] = Convert.ToInt32(action[i + 2]); // запис оцінки
 
                     students.Add(new Student(action[0], action[1], temp)); // додавання студента
                 }
         }
 
         var SelectedStudents = from s in students
-                               where s.Marks.Where(m => m == 6).Count() > 0
-                               select s;
+                               where s.Marks.Where(m => m == 6).Any() // якщо у s.Marks є хоча б одна оцінка Відмінно (6)
+                               select s; // то s записуєтся в SelectedStudents
 
         foreach (var student in SelectedStudents) // вивід студентів з SelectedStudents
             Console.WriteLine(student);
