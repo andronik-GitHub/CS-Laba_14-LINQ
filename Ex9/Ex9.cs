6df6472e401f1edef2b45a3a777ac0d8b316c8c7
kazmi <costellationoblivion@gmail.com>
2022-11-19 09:41:13 +0200

    Добавьте файлы проекта.

--- Ex9/Ex9.cs
+++ Ex9/Ex9.cs
@@ -6,7 +6,34 @@ class Ex9
 {
     static void Main()
     {
+<<<<<<< HEAD
 
+=======
+        List<Student> students = new(); // колекція студентів
+
+        while (true)
+        {
+            string[]? action = Console.ReadLine()?.Split(' '); // зчитування команди
+
+            if (action != null)
+                if (action[0].ToLower() == "end") { Console.WriteLine(); break; } // завершення вводу
+                else if (action.Length >= 2)
+                {
+                    int[] temp = new int[action.Length - 1]; // для запису оцінок
+                    for (int i = 0; i < temp.Length; i++)
+                        temp[i] = Convert.ToInt32(action[i + 1]); // запис оцінки
+
+                    students.Add(new Student(action[0], temp)); // додавання студента
+                }
+        }
+
+        var SelectedStudents = from s in students
+                               where new Regex(@"((14)|(15))$").IsMatch(s.Id) // якщо s.Id закінчується на 14 або 15
+                               select s; // то s записуєтся в SelectedStudents
+
+        foreach (var student in SelectedStudents) // вивід студентів з SelectedStudents
+            Console.WriteLine(student);
+>>>>>>> Добавьте файлы проекта.
 
 
         Console.ReadKey();
---
+++ Ex9/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Student
+{
+    public string Id;
+    public int[] Marks;
+
+    public Student(string id, int[] marks)
+    {
+        Id = id;
+        Marks = marks;
+    }
+
+    public override string ToString() => string.Join(' ', Marks);
+}
